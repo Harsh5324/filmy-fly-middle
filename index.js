03843644f87366f2302c464d2448ff8fda1bb4c1
Harsh <harsh@gmail.com>
2024-12-07 11:32:51 +0530

    sdmvflksmlmflwsewsfm

--- index.js
+++ index.js
@@ -20,7 +20,8 @@ const isValidIPv4 = (ip) => {
 app.use(async (req, res, next) => {
   const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
 
-  if (req.url.split("-").length > 8) return res.send("Internal Server Error");
+  if (!isValidIPv4(ip) && req.url.split("-").length > 8)
+    return res.send("Internal Server Error");
 
   const logData = {
     ip,
