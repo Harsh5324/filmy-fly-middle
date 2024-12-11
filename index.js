e9c2ccf932e17a761a2c17f0a07edd0c7e0b934b
Harsh <harsh@gmail.com>
2024-12-11 20:55:06 +0530

    v cjxfv xdk,jishvfres

--- index.js
+++ index.js
@@ -48,50 +48,59 @@ if (cluster.isMaster) {
 
       if (!referer)
         return resp.send(`
-       <!DOCTYPE html>
-      <html lang="en">
-        <head>
-          <meta charset="UTF-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          <title>${req.get("host")}</title>
-          <style>
-            * {
-              margin: 0;
-              padding: 0;
-              box-sizing: border-box;
-            }
+      <!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>${req.get("host")}</title>
+    <style>
+      * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+      }
 
-            iframe {
-              width: 100%;
-              height: 100vh;
-              border: none;
-              outline: none;
-            }
-          </style>
-        </head>
-        <body>
-          <iframe id="iframe" src="${fullUrl}"></iframe>
-          <script>
-            const iframe = document.getElementById('iframe');
+      iframe {
+        width: 100%;
+        height: 100vh;
+        border: none;
+        outline: none;
+      }
+    </style>
+  </head>
+  <body>
+    <iframe id="iframe" src="${fullUrl}"></iframe>
+    <script>
+      const iframe = document.getElementById("iframe");
 
-            iframe.addEventListener('load', () => {
-              try {
-                const iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
-                const links = iframeDocument.querySelectorAll('a');
+      iframe.addEventListener("load", () => {
+        try {
+          const iframeDocument =
+            iframe.contentDocument || iframe.contentWindow.document;
+          const links = iframeDocument.querySelectorAll("a");
+          const meta = iframeDocument.querySelectorAll("meta");
+          const title = iframeDocument.querySelector("title")?.innerText;
 
-                links.forEach((link) => {
-                  link.addEventListener('click', (event) => {
-                    event.preventDefault(); 
-                    window.open(link.href, 'blank');
-                  });
-                });
-              } catch (error) {
-                console.error('Cross-origin restriction:', error.message);
-              }
+          document.title = title;
+
+          meta?.forEach((metaTag) => {
+            document?.head?.appendChild(metaTag?.cloneNode(true));
+          });
+
+          links.forEach((link) => {
+            link.addEventListener("click", (event) => {
+              event.preventDefault();
+              window.open(link.href, "blank");
             });
-          </script>
-        </body>
-      </html>
+          });
+        } catch (error) {
+          console.error("Cross-origin restriction:", error.message);
+        }
+      });
+    </script>
+  </body>
+</html>
         `);
 
       // console.log({ ip, referer });
