cda5af6499b862bf75ca57b625bf1c965f51190b
Harsh <harsh@gmail.com>
2024-12-16 10:58:06 +0530

    ialfjnlweq33qwerw

--- index.js
+++ index.js
@@ -38,53 +38,53 @@ app.use(async (req, resp) => {
     if (userAgent.includes("Dalvik") || req.url.includes(".html.html"))
       return resp.send("Invalid activity");
 
-    if (!referer && !isGoogleBot)
-      return resp.send(`
-        <!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="UTF-8" />
-<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-<title>${req.get("host")}</title>
-<style>
-* { margin: 0; padding: 0; box-sizing: border-box; }
-iframe { width: 100%; height: 100vh; border: none; outline: none; }
-</style>
-</head>
-<body>
-<iframe id="iframe"></iframe>
-<script>
-const iframe = document.getElementById("iframe");
-window.onload = () => {
-  setTimeout(() => {
-    iframe.src = "${fullUrl}"
-  },500)
-}
-iframe.addEventListener("load", () => {
-try {
-const iframeDocument =
-iframe.contentDocument || iframe.contentWindow.document;
-const links = iframeDocument.querySelectorAll("a");
-const meta = iframeDocument.querySelectorAll("meta");
-const title = iframeDocument.querySelector("title")?.innerText;
-if (title) document.title = title;
-meta?.forEach((metaTag) => {
-document?.head?.appendChild(metaTag?.cloneNode(true));
-});
-links.forEach((link) => {
-link.addEventListener("click", (event) => {
-event.preventDefault();
-window.open(link.href, "_blank");
-});
-});
-} catch (error) {
-console.log("Cross-origin restriction:", error.message);
-}
-});
-</script>
-</body>
-</html>
-      `);
+    //     if (!referer && !isGoogleBot)
+    //       return resp.send(`
+    //         <!DOCTYPE html>
+    // <html lang="en">
+    // <head>
+    // <meta charset="UTF-8" />
+    // <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    // <title>${req.get("host")}</title>
+    // <style>
+    // * { margin: 0; padding: 0; box-sizing: border-box; }
+    // iframe { width: 100%; height: 100vh; border: none; outline: none; }
+    // </style>
+    // </head>
+    // <body>
+    // <iframe id="iframe"></iframe>
+    // <script>
+    // const iframe = document.getElementById("iframe");
+    // window.onload = () => {
+    //   setTimeout(() => {
+    //     iframe.src = "${fullUrl}"
+    //   },500)
+    // }
+    // iframe.addEventListener("load", () => {
+    // try {
+    // const iframeDocument =
+    // iframe.contentDocument || iframe.contentWindow.document;
+    // const links = iframeDocument.querySelectorAll("a");
+    // const meta = iframeDocument.querySelectorAll("meta");
+    // const title = iframeDocument.querySelector("title")?.innerText;
+    // if (title) document.title = title;
+    // meta?.forEach((metaTag) => {
+    // document?.head?.appendChild(metaTag?.cloneNode(true));
+    // });
+    // links.forEach((link) => {
+    // link.addEventListener("click", (event) => {
+    // event.preventDefault();
+    // window.open(link.href, "_blank");
+    // });
+    // });
+    // } catch (error) {
+    // console.log("Cross-origin restriction:", error.message);
+    // }
+    // });
+    // </script>
+    // </body>
+    // </html>
+    //       `);
 
     console.log("After", {
       url: fullUrl,
